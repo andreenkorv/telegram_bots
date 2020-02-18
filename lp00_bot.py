451392a319efb514ef64ce3e46431056aa70666b
Roman <41366776+andreenkorv@users.noreply.github.com>
2020-02-18 09:10:12 +0300

    Update lp00_bot.py
    Token

--- lp00_bot.py
+++ lp00_bot.py
@@ -4,7 +4,7 @@ PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 def main():
-    LP00_bot = Updater("1008440417:AAFPWHZXGgQ5zeFOkSwdCll0z8LF_xemDcU", use_context=True, request_kwargs=PROXY)
+    LP00_bot = Updater("TOKEN", use_context=True, request_kwargs=PROXY)
     LP00_bot.start_polling()
     LP00_bot.idle()
 
